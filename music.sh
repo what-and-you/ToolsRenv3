846ba349ad184fc9fab7a00f1e41f2e4ebe2a63c
ewepaksa123 <sejatipria990@gmail.com>
2024-06-03 14:42:27 +0700

    Update music.sh

--- music.sh
+++ music.sh
@@ -3,7 +3,3 @@ music_file="mpv $HOME/menu/AUD-20240525-WA0355.mp3 &>/dev/null &"
 
 # Putar musik
 omxplayer "$mpv $HOME/menu/AUD-20240525-WA0355.mp3 &>/dev/null &"
-clear
-chmod +x script.sh
-./script.sh
-play
