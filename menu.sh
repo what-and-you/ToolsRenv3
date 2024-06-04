4bf3d91f2d5e6107c36da7d03b893b2b8a827b4c
ewepaksa123 <sejatipria990@gmail.com>
2024-06-04 20:08:15 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -40,9 +40,10 @@ sleep 3
 clear
 echo "FOLLOW AKUN TIKTOK GW WAK"
 sleep 3
-xdg-open "https://www.tiktok.com/@sistem9999?_t=8mE62usv4Br&_r=1"
 clear
+xdg-open "https://www.tiktok.com/@sistem9999?_t=8mE62usv4Br&_r=1"
 sleep 6
+clear
 echo "SELAMAT DATANG DI TOOLSRENV2☺"
 sleep 4
 clear
