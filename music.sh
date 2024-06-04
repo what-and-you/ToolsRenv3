1c8f1771e79979080d61908cfda62d31663742d3
ewepaksa123 <sejatipria990@gmail.com>
2024-06-04 13:55:37 +0700

    Delete music.sh

--- music.sh
+++
@@ -1,9 +0,0 @@
-# Jalur file musik
-music_file="mpv $HOME/menu/AUD-20240525-WA0355.mp3 &>/dev/null &"
-
-# Putar musik
-mpg321 "$music_files=("music1.mp3" "music2.mp3" "music3.mp3" "music.mp3")   "
-clear
-chmod +x putar_musik.sh
-clear
-./putar_musik.sh
